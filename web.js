b208a1239f1aea434d6ec97fb0c97a5f9d12b224
Tim Wang <shtimwang@gmail.com>
2013-09-22 15:10:48 +0000

    Debug 3 web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var buf = fs.readFileSync(index.html);
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString);
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
